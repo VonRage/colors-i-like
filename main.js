fc9d042f13f39b7cdf7dce2590eddfe7dcc005f5
Minnervable <267438469+Minnervable@users.noreply.github.com>
2026-05-30 19:43:31 -0400

    feat: implement color input and local storage persistence with dynamic swatch rendering

--- main.js
+++ main.js
@@ -1,29 +1,78 @@
-var test1Colors = [
-	{ name: "Oxford Blue", hex: "#070F34" },
-	{ name: "Zaffre", hex: "#0313A6" },
-	{ name: "Dark violet", hex: "#9201CB" },
-	{ name: "Hollywood cerise", hex: "#F715AB" },
-	{ name: "Fluorescent cyan", hex: "#34EDF3" }
-];
-
-function createTable() {
-	var headers = ["Name", "Hex Value"];
-	var table = document.createElement("TABLE");
-
-	for(var i = 0; i < test1Colors.length; i++) {
-		var row = table.insertRow(i);
-		row.insertCell(0).innerHTML = test1Colors[i].name;
-		row.insertCell(1),innerHTML = test1Colors[i].hex;
-	}
-
-	var header = table.createTHead();
-	var headerRow = header.insertRow(0);
-	for(var i = 0; i < headers.length; i++) {
-		headerRow.insertCell(i),innerHTML = headers[i];
-	}
-
-	var tableDiv = document.getElementByID("tableDisplay");
-	document.tableDisplay.append(table);
+// global variables and initilizations
+// this line will retrieve the saved array from local storage
+let colorsILike;
+if (localStorage.getItem("colorsILike") !== null) {
+  colorsILike = JSON.parse(localStorage.getItem("colorsILike"));
+} else {
+  colorsILike = [];
 }
 
-createTable();
+let color;
+// push input box to array
+// transfer and clean string to useable data
+let input = document.querySelector("#input");
+let addColor = document.querySelector("#submit");
+let mainDisplay = document.querySelector("#colors-display");
+
+addColor.addEventListener("click", function () {
+  color = input.value.toLowerCase();
+  color = color.trim();
+  if (color === "") {
+    alert("Please enter a color");
+    return;
+  }
+  if (colorsILike.includes(color)) {
+    alert("You already have this color");
+    return;
+  }
+  colorsILike.push(color);
+    alert(`You added ${color} to your list!`);
+  input.value = "";
+
+  mainDisplay.textContent = ""
+  
+  colorsILike.forEach(color => {
+    let container = document.createElement("div");
+    container.classList.add("swatch-container");  
+    let swatch = document.createElement("div");
+    let header = document.createElement("h3");
+    swatch.classList.add("swatch");
+    swatch.style.backgroundColor = color;
+    header.textContent = color;
+    container.appendChild(header);
+    container.appendChild(swatch);
+    mainDisplay.appendChild(container); 
+  });
+    localStorage.setItem("colorsILike", JSON.stringify(colorsILike)); 
+
+});
+
+// let test1Colors = [
+// 	{ name: "Oxford Blue", hex: "#070F34" },
+// 	{ name: "Zaffre", hex: "#0313A6" },
+// 	{ name: "Dark violet", hex: "#9201CB" },
+// 	{ name: "Hollywood cerise", hex: "#F715AB" },
+// 	{ name: "Fluorescent cyan", hex: "#34EDF3" },
+// ];
+
+// function createTable() {
+// 	let headers = ["Name", "Hex Value"];
+// 	let table = document.createElement("TABLE");
+
+// 	for (let i = 0; i < test1Colors.length; i++) {
+// 		let row = table.insertRow(i);
+// 		row.insertCell(0).innerHTML = test1Colors[i].name;
+// 		(row.insertCell(1), (innerHTML = test1Colors[i].hex));
+// 	}
+
+// 	let header = table.createTHead();
+// 	let headerRow = header.insertRow(0);
+// 	for (let i = 0; i < headers.length; i++) {
+// 		(headerRow.insertCell(i), (innerHTML = headers[i]));
+// 	}
+
+// 	let tableDiv = document.getElementByID("tableDisplay");
+// 	document.tableDisplay.append(table);
+// }
+
+// createTable();
