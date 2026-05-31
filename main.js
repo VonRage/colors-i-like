6152ee4c6a7df2a3ec883077163dd3154751bc6b
Minnervable <267438469+Minnervable@users.noreply.github.com>
2026-05-31 00:28:45 -0400

    Add dropdown menu

--- main.js
+++ main.js
@@ -1,29 +1,84 @@
-var test1Colors = [
-	{ name: "Oxford Blue", hex: "#070F34" },
-	{ name: "Zaffre", hex: "#0313A6" },
-	{ name: "Dark violet", hex: "#9201CB" },
-	{ name: "Hollywood cerise", hex: "#F715AB" },
-	{ name: "Fluorescent cyan", hex: "#34EDF3" }
-];
-
-function createTable() {
-	var headers = ["Name", "Hex Value"];
-	var table = document.createElement("TABLE");
-
-	for(var i = 0; i < test1Colors.length; i++) {
-		var row = table.insertRow(i);
-		row.insertCell(0).innerHTML = test1Colors[i].name;
-		row.insertCell(1),innerHTML = test1Colors[i].hex;
-	}
-
-	var header = table.createTHead();
-	var headerRow = header.insertRow(0);
-	for(var i = 0; i < headers.length; i++) {
-		headerRow.insertCell(i),innerHTML = headers[i];
-	}
-
-	var tableDiv = document.getElementByID("tableDisplay");
-	document.tableDisplay.append(table);
+// global variables and initilizations
+// this line will retrieve the saved array from local storage
+let colorsILike;
+if (localStorage.getItem("colorsILike") !== null) {
+	colorsILike = JSON.parse(localStorage.getItem("colorsILike"));
+} else {
+	colorsILike = [];
 }
 
-createTable();
+let color;
+// push input box to array
+// transfer and clean string to useable data
+let input = document.querySelector("#input");
+let addColor = document.querySelector("#submit");
+let mainDisplay = document.querySelector("#colors-display");
+let swatchBox = document.querySelector(".swatch-box");
+
+
+
+addColor.addEventListener("click", function () {
+	color = input.value.toLowerCase();
+	color = color.trim();
+	if (color === "") {
+		alert("Please enter a color");
+		return;
+	}
+	if (colorsILike.includes(color)) {
+		alert("You already have this color");
+		return;
+	}
+	colorsILike.push(color);
+	alert(`You added ${color} to your list!`);
+	input.value = "";
+
+	mainDisplay.textContent = ""
+
+	colorsILike.forEach(color => {
+		const swatchContainer = document.createElement("div");
+		swatchContainer.classList.add("swatch-container");
+		const swatch = document.createElement("div");
+		const header = document.createElement("h3");
+		swatch.classList.add("swatch");
+		swatch.style.backgroundColor = color;
+		header.textContent = color;
+		swatchContainer.appendChild(header);
+		swatchContainer.appendChild(swatch);
+		const dropDownMenu = document.createElement("select");
+		const menuItem = ["Delete Swatch", "Add Color Name", "Move Swatch", "Edit Swatch"]
+		menuItem.forEach(item => {
+			const option = document.createElement("option");
+			option.textContent = item;
+			dropDownMenu.appendChild(option);
+
+			dropDownMenu.addEventListener("change", function () {
+				let action = dropDownMenu.value;
+				switch (action) {
+					case "Delete Swatch":
+						colorsILike.splice(colorsILike.indexOf(color), 1);
+						break;
+					case "Add Color Name":
+
+						break;
+					case "Move Swatch":
+
+						break;
+					case "Edit Swatch":
+
+						break;
+					default:
+						break;
+				}
+			})
+		})
+		swatchContainer.appendChild(dropDownMenu);
+		mainDisplay.appendChild(swatchContainer);
+
+
+
+	});
+	localStorage.setItem("colorsILike", JSON.stringify(colorsILike));
+
+});
+
+
