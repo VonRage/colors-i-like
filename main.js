6f929cf19cbccc1253a319bf5ee444cce53ccf85
Minnervable <267438469+Minnervable@users.noreply.github.com>
2026-05-31 03:49:54 -0400

    Totally messed up code brought back to its former glory

--- main.js
+++ main.js
@@ -13,24 +13,78 @@ let color;
 let input = document.querySelector("#input");
 let addColor = document.querySelector("#submit");
 let mainDisplay = document.querySelector("#colors-display");
+let swatchBox = document.querySelector(".swatch-box");
+
+function displayColors() {
+
+	mainDisplay.textContent = ""
+
+	colorsILike.forEach(color => {
+		const swatchContainer = document.createElement("div");
+		swatchContainer.classList.add("swatch-container");
+		const swatch = document.createElement("div");
+		const header = document.createElement("h3");
+		swatch.classList.add("swatch");
+		swatch.style.backgroundColor = color;
+		header.textContent = color;
+		swatchContainer.appendChild(header);
+		swatchContainer.appendChild(swatch);
+		const dropDownMenu = document.createElement("select");
+		const menuItem = ["--Select --", "Delete Swatch", "Add Color Name", "Move Swatch", "Edit Swatch"]
+
+		menuItem.forEach(item => {
+			const option = document.createElement("option");
+			option.textContent = item;
+			dropDownMenu.appendChild(option);
+
+		})
+		dropDownMenu.addEventListener("change", function () {
+			let action = dropDownMenu.value;
+			switch (action) {
+				case "Delete Swatch":
+					colorsILike.splice(colorsILike.indexOf(color), 1);
+					displayColors();
+
+					break;
+				case "Add Color Name":
+
+					break;
+				case "Move Swatch":
+
+					break;
+				case "Edit Swatch":
+
+					break;
+				default:
+					break;
+			}
+		})
+		swatchContainer.appendChild(dropDownMenu);
+		mainDisplay.appendChild(swatchContainer);
+
+
+
+	});
+	localStorage.setItem("colorsILike", JSON.stringify(colorsILike));
+
+}
+
+displayColors();
 
 addColor.addEventListener("click", function () {
 	color = input.value.toLowerCase();
 	color = color.trim();
-
+	let validHexLength = 7;
 	if (color === "") {
 		alert("Please enter a color");
 		return;
-	}
-
-	if (colorsILike.includes(color)) {
+	} else if (colorsILike.includes(color)) {
 		alert("You already have this color");
 		return;
 	}
 
 	// Check hex value for valid length
-	let validHexLength = 7;
-	if (color.length > validHexLength) {
+	else if (color.length > validHexLength) {
 		alert("Please enter a valid hex color value");
 		return;
 	}
@@ -45,103 +99,10 @@ addColor.addEventListener("click", function () {
 			return;
 		}
 	}
-
-	colorsILike.push(color);
-	alert(`You added ${color} to your list!`);
-	input.value = "";
-
-	mainDisplay.textContent = "";
-
-	colorsILike.forEach((color) => {
-		let container = document.createElement("div");
-		container.classList.add("swatch-container");
-		let swatch = document.createElement("div");
-		let header = document.createElement("h3");
-		swatch.classList.add("swatch");
-		swatch.style.backgroundColor = color;
-		header.textContent = color;
-		container.appendChild(header);
-		container.appendChild(swatch);
-		mainDisplay.appendChild(container);
-	});
-
-	localStorage.setItem("colorsILike", JSON.stringify(colorsILike));
-});
-
-// let test1Colors = [
-// 	{ name: "Oxford Blue", hex: "#070F34" },
-// 	{ name: "Zaffre", hex: "#0313A6" },
-// 	{ name: "Dark violet", hex: "#9201CB" },
-// 	{ name: "Hollywood cerise", hex: "#F715AB" },
-// 	{ name: "Fluorescent cyan", hex: "#34EDF3" },
-// ];
-
-function displayColors() {
-	mainDisplay.textContent = "";
-
-	colorsILike.forEach((color) => {
-		const swatchContainer = document.createElement("div");
-		swatchContainer.classList.add("swatch-container");
-		const swatch = document.createElement("div");
-		const header = document.createElement("h3");
-		swatch.classList.add("swatch");
-		swatch.style.backgroundColor = color;
-		header.textContent = color;
-		swatchContainer.appendChild(header);
-		swatchContainer.appendChild(swatch);
-		const dropDownMenu = document.createElement("select");
-		const menuItem = [
-			"--Select --",
-			"Delete Swatch",
-			"Add Color Name",
-			"Move Swatch",
-			"Edit Swatch",
-		];
-
-		menuItem.forEach((item) => {
-			const option = document.createElement("option");
-			option.textContent = item;
-			dropDownMenu.appendChild(option);
-		});
-		dropDownMenu.addEventListener("change", function () {
-			let action = dropDownMenu.value;
-			switch (action) {
-				case "Delete Swatch":
-					colorsILike.splice(colorsILike.indexOf(color), 1);
-					displayColors();
-
-					break;
-				case "Add Color Name":
-					break;
-				case "Move Swatch":
-					break;
-				case "Edit Swatch":
-					break;
-				default:
-					break;
-			}
-		});
-		swatchContainer.appendChild(dropDownMenu);
-		mainDisplay.appendChild(swatchContainer);
-	});
-	localStorage.setItem("colorsILike", JSON.stringify(colorsILike));
-}
-
-displayColors();
-
-addColor.addEventListener("click", function () {
-	color = input.value.toLowerCase();
-	color = color.trim();
-	if (color === "") {
-		alert("Please enter a color");
-		return;
-	}
-	if (colorsILike.includes(color)) {
-		alert("You already have this color");
-		return;
-	}
 	colorsILike.push(color);
 	// alert(`You added ${color} to your list!`);
 	input.value = "";
 	displayColors();
 });
+
+
